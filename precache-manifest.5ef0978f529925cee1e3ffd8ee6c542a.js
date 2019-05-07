28180e83db8bc72a91300fd24f5dec6de9b5819b
Larysa <Larysa76@gmail.com>
2019-05-07 22:25:15 +0300

    Updates

---
+++ precache-manifest.5ef0978f529925cee1e3ffd8ee6c542a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27bc5ebc0857a09d0a76fd7b6f0686a3",
+    "url": "/goit-react-hw-01/index.html"
+  },
+  {
+    "revision": "66bda72fee63ae407201",
+    "url": "/goit-react-hw-01/static/css/main.53c96ba5.chunk.css"
+  },
+  {
+    "revision": "daea71af69a1c43f7327",
+    "url": "/goit-react-hw-01/static/js/2.242fd1b4.chunk.js"
+  },
+  {
+    "revision": "66bda72fee63ae407201",
+    "url": "/goit-react-hw-01/static/js/main.44d35bbe.chunk.js"
+  },
+  {
+    "revision": "f51632aec18d5636ad74",
+    "url": "/goit-react-hw-01/static/js/runtime~main.bdf556df.js"
+  }
+]);
